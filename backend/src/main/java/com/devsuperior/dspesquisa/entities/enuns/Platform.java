27007dd1ba8a5e9b65e1a33703d3e0431de467d9
Raífe Ferreira <raife.tec@gmail.com>
2020-09-15 01:54:57 -0300

    Entities, Repositories

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/enuns/Platform.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dspesquisa.entities.enuns;
+
+public enum Platform {
+	
+	
+	PC,
+	PLAYSTATION,
+	XBOX;
+	
+	
+
+}
---
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/GanreRepository.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dspesquisa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dspesquisa.entities.Genre;
+
+public interface GanreRepository extends JpaRepository<Genre, Long> {
+	
+	
+	
+	
+
+}
