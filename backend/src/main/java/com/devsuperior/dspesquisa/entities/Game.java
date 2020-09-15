ed61c33987c91d5be98c2051debd58aea192eb07
Raífe Ferreira <raife.tec@gmail.com>
2020-09-15 02:30:33 -0300

    implement DTO and Services

--- backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Game.java
@@ -30,7 +30,7 @@ public class Game implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "genre_id")
 	private Genre genre;
-	
+
 	@OneToMany(mappedBy = "game")
 	private List<Record> records = new ArrayList<>();
 	
